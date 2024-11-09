e8dd92952350ccdf4f44ae3ad2c38b397b33a9dd
BennyLi <bennyli2011@163.com>
2024-11-09 16:46:49 +0800

    移除对 Timer 的依赖

--- action/src/main/java/org/betastudio/application/action/utils/SleepingAction.java
+++ action/src/main/java/org/betastudio/application/action/utils/SleepingAction.java
@@ -2,19 +2,22 @@ package org.betastudio.application.action.utils;
 
 
 import org.betastudio.application.action.Action;
-import org.betastudio.application.action.Timer;
 
 public final class SleepingAction implements Action {
-	private final Timer timer=new Timer();
 	private final long sleepMilliseconds;
+	private double startTime;
+	private boolean initialized=false;
 
 	public SleepingAction(final long sleepMilliseconds){
 		this.sleepMilliseconds = sleepMilliseconds;
-		timer.restart();
 	}
 
 	@Override
 	public boolean run() {
-		return timer.stopAndGetDeltaTime()< sleepMilliseconds;
+		if(!initialized){
+			startTime=System.nanoTime()/1e6;
+			initialized=true;
+		}
+		return System.nanoTime()/1e6-startTime < sleepMilliseconds;
 	}
 }
