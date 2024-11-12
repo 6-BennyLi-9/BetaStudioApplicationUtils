250cf6acce0d7ca122f3edff9f9e61d0b68f0c4a
BennyLi <bennyli2011@163.com>
2024-11-12 19:06:52 +0800

    测试 TaggedActionPackage

--- action/src/test/java/MainTestingArea.java
+++ action/src/test/java/MainTestingArea.java
@@ -1,5 +1,29 @@
-public class MainTestingArea {
-	public static void main(String[] args) {
+import org.betastudio.application.action.Action;
+import org.betastudio.application.action.packages.TaggedActionPackage;
 
+public class MainTestingArea {
+	public static class PrintAction implements Action {
+		private final long printVal;
+
+		public PrintAction(){
+			printVal=0;
+		}
+		public PrintAction(long printVal){
+			this.printVal=printVal;
+		}
+
+		@Override
+		public boolean run() {
+			System.out.println(printVal);
+			return false;
+		}
+	}
+
+	public static void main(String[] args) {
+		TaggedActionPackage thread=new TaggedActionPackage();
+		thread.add("print1",new PrintAction(10));
+		thread.add("print2",new PrintAction(20));
+
+		System.out.println(thread.run());
 	}
 }
