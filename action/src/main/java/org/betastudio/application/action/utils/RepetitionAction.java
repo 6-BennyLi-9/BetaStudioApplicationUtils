2f3c7e3fad6eba5f97d6efa97470039d2cbeb2e8
BennyLi <bennyli2011@163.com>
2024-11-16 15:51:58 +0800

    加入 RepeatAction

--- action/src/main/java/org/betastudio/application/action/utils/RepetitionAction.java
+++ action/src/main/java/org/betastudio/application/action/utils/RepetitionAction.java
@@ -3,10 +3,13 @@ package org.betastudio.application.action.utils;
 import org.betastudio.application.action.Action;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * 类版本的 {@link RepeatAction} ，但需要提供要执行的 {@code Action} 块
+ */
 public final class RepetitionAction implements Action {
 	private final long times;
 	private long ptr;
-	private Action argument;
+	private final Action argument;
 
 	public RepetitionAction(final Action repeatArgument, final long times){
 		this.times=times;
