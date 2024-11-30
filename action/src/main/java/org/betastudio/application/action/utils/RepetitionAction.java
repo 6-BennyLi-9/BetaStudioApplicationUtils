ebe454c271638aed9e12570479ac84a7acedf2bd
BennyLi <bennyli2011@163.com>
2024-11-30 11:10:18 +0800

    更新代码

--- action/src/main/java/org/betastudio/application/action/utils/RepetitionAction.java
+++ action/src/main/java/org/betastudio/application/action/utils/RepetitionAction.java
@@ -7,24 +7,27 @@ import org.jetbrains.annotations.NotNull;
  * 类版本的 {@link RepeatAction} ，但需要提供要执行的 {@code Action} 块
  */
 public final class RepetitionAction implements Action {
-	private final long times;
-	private long ptr;
+	private final long   times;
+	private       long   ptr;
 	private final Action argument;
 
-	public RepetitionAction(final Action repeatArgument, final long times){
-		this.times=times;
-		argument=repeatArgument;
+	public RepetitionAction(final Action repeatArgument, final long times) {
+		this.times = times;
+		argument = repeatArgument;
 	}
 
 	@Override
 	public boolean run() {
-		boolean res=argument.run();
-		return res && ptr++<times;
+		final boolean res = argument.run();
+		if (! res) return false;
+		final boolean b = ptr < times;
+		ptr++;
+		return b;
 	}
 
 	@Override
 	@NotNull
 	public String paramsString() {
-		return "/"+times+"/"+argument.paramsString();
+		return "/" + times + "/" + argument.paramsString();
 	}
 }
